4fa4abf2d88b59e301ce2f9c13a298f4b5cc2151
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-01-02 14:31:15 -0600

    move old Helpy to new repo

--- zzzPyPI.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-pip install -e .
-# or:
-# python setup.py develop
-python setup.py sdist bdist_wheel --universal
-python setup.py register
-twine upload dist/*
-# or:
-# python setup.py sdist bdist_wheel upload
