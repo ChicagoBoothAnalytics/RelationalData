bdba1dfc13607633a467f7de5f4c0fe4b9250f0b
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-01-17 02:19:38 -0600

    add SQL script to generate age_from and age_to from age_bucket

---
+++ Cases/Kaggle AirBnB/SQL/AGDstats_generate_agefrom_and_ageto_from_agebucket.sql
@@ -0,0 +1,59 @@
+-- generate "age_from" & "age_to" from "age_bucket"
+
+DROP TABLE IF EXISTS age_gender_destination_stats_with_age_from_to;
+
+SELECT
+    age_bucket,
+    CAST(age_from_to[1] AS DOUBLE PRECISION) - 0.5 AS age_from,
+    CAST(age_from_to[2] AS DOUBLE PRECISION) + 0.5 AS age_to,
+    gender,
+    country_destination,
+    population_in_thousands
+  INTO
+    TEMP TABLE age_gender_destination_stats_with_age_from_to
+  FROM
+    (SELECT
+        age_bucket,
+        CASE WHEN age_bucket = '100+' THEN ARRAY['100', '100']
+             ELSE REGEXP_MATCHES(age_bucket, '([0-9]*)-([0-9]*)')
+                  -- alternatives in this case:
+                  -- REGEXP_SPLIT(age_bucket, '-') or
+                  -- STRING_TO_ARRAY(age_bucket, '-')
+             END
+             AS age_from_to,
+        gender,
+        country_destination,
+        population_in_thousands
+      FROM
+        age_gender_destination_stats) subq
+  ORDER BY
+    age_from,
+    gender,
+    country_destination;
+
+SELECT
+    *
+  FROM
+      age_gender_destination_stats_with_age_from_to;
+
+
+-- obtain definitions of distinct age buckets
+
+DROP TABLE IF EXISTS age_buckets;
+
+SELECT
+    DISTINCT
+      age_bucket,
+      age_from,
+      age_to
+  INTO
+    TEMP TABLE age_buckets
+  FROM
+    age_gender_destination_stats_with_age_from_to
+  ORDER BY
+    age_from;
+
+SELECT
+    *
+  FROM
+    age_buckets;
