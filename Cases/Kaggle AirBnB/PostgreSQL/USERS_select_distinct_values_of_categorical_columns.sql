5bb7fd9cb10272996ce6b122bf6929610ec555b6
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-01-22 22:51:12 -0600

    add illustration of DISTINCT

---
+++ Cases/Kaggle AirBnB/PostgreSQL/USERS_select_distinct_values_of_categorical_columns.sql
@@ -0,0 +1,64 @@
+SELECT
+    DISTINCT gender
+  FROM
+    users;
+
+
+SELECT
+    DISTINCT language
+  FROM
+    users;
+
+
+SELECT
+    DISTINCT signup_method
+  FROM
+    users;
+
+
+SELECT
+    DISTINCT signup_flow
+  FROM
+    users;
+
+
+SELECT
+    DISTINCT affiliate_channel
+  FROM
+    users;
+
+
+SELECT
+    DISTINCT affiliate_provider
+  FROM
+    users;
+
+
+SELECT
+    DISTINCT first_affiliate_tracked
+  FROM
+    users;
+
+
+SELECT
+    DISTINCT signup_app
+  FROM
+    users;
+
+
+SELECT
+    DISTINCT first_device_type
+  FROM
+    users;
+
+
+SELECT
+    DISTINCT first_browser
+  FROM
+    users;
+
+
+SELECT
+    DISTINCT country_destination
+  FROM
+    users;
